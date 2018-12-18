397282674cf12c6c2d8d2216c0ef4ac42fcf8f21
Lepimond <megaobla@gmail.com>
2018-12-18 20:50:37 +1000

    Added RecordBaseHelper to make records' base

---
+++ app/src/main/java/com/example/evgsa/androidgraphics/utils/RecordBaseHelper.java
@@ -0,0 +1,89 @@
+package com.example.evgsa.androidgraphics.utils;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class RecordBaseHelper extends SQLiteOpenHelper
+{
+    public static final String DATABASE_NAME = "records_base";
+    public static final int DATABASE_VERSION = 1;
+
+    public static final String TABLE_NAME = "records_table";
+    public static final String KEY_ID = "_id";
+    public static final String KEY_RECORD = "record";
+    public static final String KEY_TIMESTAMP = "timestamp";
+
+    public RecordBaseHelper(Context context)
+    {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db)
+    {
+        db.execSQL("create table " + TABLE_NAME + "(" +
+                                                   KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                                                   KEY_RECORD + " integer," +
+                                                   KEY_TIMESTAMP  + " TIMEDATE DEFAULT CURRENT_TIMESTAMP)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+        db.execSQL("drop table if exists " + TABLE_NAME);
+        onCreate(db);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
